7408db1ec5faee7264729bc5f549731c6ce3b4b9
konoha81 <f_romi81@yahoo.co.jp>
2017-11-06 22:15:57 +0900

    add EXT

--- MLmodels.py
+++ MLmodels.py
@@ -2,22 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.externals import joblib
 from sklearn.preprocessing import Imputer
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+import xgboost as xgb
 import pandas as pd
 import datetime, os
 
-import xgboost as xgb
 
 class MLmodels(object):
     """
     機械学習モデルのセット。
-    作成済み：XGboost, RandomForest
-    （作成予定：SVM, Logistic regression)
+    作成済み：XGboost, RandomForest, ExtraTree,
+    （追加作成予定：SVM, Logistic regression)
     """
 
     def __init__(self, X_train, X_test, y_train, y_test):
@@ -97,6 +98,68 @@ class MLmodels(object):
                           n_jobs= -1)
 
         return clf
+
+
+    def EXTmodel(self, test=False):
+        # Extremely Randomized Trees
+
+        # GridSearch用パラメータ
+        parameters = {
+                'n_estimators'      : [700, 1000, 1250, 1500, 2000],
+                'max_features'      : [1, 3, 10],
+                'random_state'      : [0],
+                'n_jobs'            : [-1],
+                'min_samples_split' : [3, 5, 20],
+                'max_depth'         : [3, 5, 20, 30],
+                'class_weight'      : ['balanced']
+        }
+
+        # テスト用
+        test_parameters = {
+                'n_estimators'      : [1500],
+                'max_features'      : ['auto'],
+                'random_state'      : [0],
+                'n_jobs'            : [-1],
+                'min_samples_split' : [5],
+                'max_depth'         : [20],
+                'class_weight'      : ['balanced']
+        }
+
+        if test==True:
+            parameters = test_parameters
+
+        # GridSearchを実行
+        clf = GridSearchCV(estimator= ExtraTreesClassifier(),
+                           param_grid= parameters,
+                          scoring= 'accuracy',
+                          cv= 10,
+                          n_jobs= -1)
+
+        return clf
+
+    def LGmodel(self, test=False):
+        # Logistic Regression
+
+        ### 数値の正規化をする
+        print('Unimplemented')
+        sys.exit()
+
+        parameters = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000] }
+
+        test_parameters = {'C': [0.001, 0.01] }
+
+        if test==True:
+            parameters = test_parameters
+
+        # GridSearchを実行
+        clf = GridSearchCV(estimator= LogisticRegression(),
+                           param_grid= parameters,
+                          scoring= 'accuracy',
+                          cv= 10,
+                          n_jobs= -1)
+
+        return clf
+
 
     def fit_and_prediction(self, clf, save=False):
         """
